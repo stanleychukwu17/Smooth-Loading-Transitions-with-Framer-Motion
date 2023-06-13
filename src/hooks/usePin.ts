3ce6b372e27bf38c712862dbb697a9e45c63ca30
Chukwu Stanley <stanleychukwu17@yahoo.com>
2023-06-13 20:52:00 +0100

    fixed the bug with my custom scroll trigger, looking too frosh nowgit add .

--- src/hooks/usePin.ts
+++ src/hooks/usePin.ts
@@ -1,4 +1,5 @@
 import { useScroll, useTransform, MotionValue, useMotionValue, useMotionValueEvent } from 'framer-motion';
+import { gsap } from 'gsap';
 
 type usePinProps = {
     itemToUpdate: MotionValue<number>,
@@ -11,18 +12,28 @@ export const usePin = (props: usePinProps): MotionValue<number> => {
     const dGoTo = props.distanceToPin
     const {scrollY} = useScroll()
     let newYPos = useMotionValue(0)
+    let currentNumber = 0;
 
     useTransform(scrollY, (currentScrollY) => { // inCharge of pining the phone down on user scroll
+        currentNumber = 0
         if (currentScrollY > dTop && currentScrollY <= dGoTo) {
-            newYPos.set(currentScrollY - dTop)
+            gsap.set('div.DtsMidCvr', {position: 'fixed'})
+            currentNumber = 0 - dTop;
         } else if (currentScrollY > dGoTo) {
-            newYPos.set(dGoTo - dTop)
+            gsap.set('div.DtsMidCvr', {position: 'relative'})
+            currentNumber = dGoTo - dTop
+        } else {
+            currentNumber = 0
+            gsap.set('div.DtsMidCvr', {position: 'relative'})
         }
+
+        console.log({currentScrollY, dGoTo, 'ePass':currentScrollY > dGoTo, currentNumber})
+        newYPos.set(currentNumber)
     })
 
     // this event below will automatically unsubscribe when the component is unmounted
     useMotionValueEvent(newYPos, 'change', (latest) => {
-        itemToUpdate.set(latest)
+        itemToUpdate.set(currentNumber)
     })
 
     return newYPos
